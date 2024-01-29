22b8a2db0a2f43578f24099dd6deca42db68177f
kamron <kamrontoxirov001@gmail.com>
2024-01-29 18:43:01 +0500

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,59 @@
+from django.contrib import messages
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+from accounts.forms import UserSignupForm, UserSigninForm
+
+
+# Create your views here.
+def user_register(request):
+    if request.method == 'POST':
+        form = UserSignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.set_password(user.password1)
+            user.is_active = True
+            form = user.save()
+
+            if form is not None:
+                print('User created successfully!')
+            else:
+                print('User not created')
+
+            return redirect('login')
+    else:
+        form = UserSignupForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/register.html', context=context)
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserSigninForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+
+            print(username, password)
+            user = authenticate(request, username=username, password=password)
+            print(user)
+            if user is not None:
+                login(request, user)
+                return redirect('news_page')
+            else:
+                messages.error(request, 'Username or password is incorrect')
+                return redirect('login')
+    else:
+        form = UserSigninForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('dashboard')
