bd397c70489f53747122e79a1ad1e51e39706f2c
kamron <kamrontoxirov001@gmail.com>
2024-01-31 18:21:37 +0500

    second commit

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import news_page
+from .views import news_page, category, contact
 
 urlpatterns = [
-    path('', news_page, name='news_page')
+    path('', news_page, name='news_page'),
+    path('category/', category, name='category'),
+    path('contact/', contact, name='contact'),
+    # path('single/', single, name='single'),
 
 ]
--- news/views.py
+++ news/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def news_page(request):
     return render(request, 'news/index.html')
+
+
+def category(request):
+    return render(request, 'news/category.html')
+
+
+def contact(request):
+    return render(request, 'news/contact.html')
